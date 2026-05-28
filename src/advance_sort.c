50c1ea367c079c9ac14579bca1891bf560407087
virzimayhandsyahputra <virzi@mhs.usk.ac.id>
2026-05-28 20:52:37 +0700

    sedikit modifikasi merge sort

--- src/advance_sort.c
+++ src/advance_sort.c
@@ -10,16 +10,16 @@
 static char _merge_L[MAX_WORDS / 2 + 2][MAX_WORD_LEN];
 static char _merge_R[MAX_WORDS / 2 + 2][MAX_WORD_LEN];
 
-/* Fungsi merge internal: gabungkan arr[left..mid] dan arr[mid+1..right] */
+// Fungsi merge internal: mengabungkan arr[left..mid] dan arr[mid+1..right]
 static void merge(char arr[][MAX_WORD_LEN], int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
-    /* Salin ke buffer sementara */
+    // Salin ke buffer sementara
     for (int i = 0; i < n1; i++) strcpy(_merge_L[i], arr[left + i]);
     for (int j = 0; j < n2; j++) strcpy(_merge_R[j], arr[mid + 1 + j]);
 
-    /* Gabungkan kembali ke arr[] secara terurut */
+    // Gabungkan kembali ke arr[] dengan terurut
     int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
         if (strcmp(_merge_L[i], _merge_R[j]) <= 0)
@@ -29,4 +29,14 @@ static void merge(char arr[][MAX_WORD_LEN], int left, int mid, int right) {
     }
     while (i < n1) strcpy(arr[k++], _merge_L[i++]);
     while (j < n2) strcpy(arr[k++], _merge_R[j++]);
+}
+
+void merge_sort(char arr[][MAX_WORD_LEN], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2; 
+        // recursive
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
 }
