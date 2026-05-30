c4745b6d9e69cf08e9dc0a2d4004bd12df9da17a
wazir655 <ahmadwazir@mhs.usk.ac.id>
2026-05-30 15:11:35 +0700

    Update basic_sort.h

--- src/basic_sort.h
+++ src/basic_sort.h
@@ -1,1 +1,11 @@
+#ifndef BASIC_SORTING_H
+#define BASIC_SORTING_H
+
+void bubble_sort(int arr[], int n);
+void insertion_sort(int arr[], int n);
+void selection_sort(int arr[], int n);
+
+void menu_sorting_dasar(void);
+
+#endif
 
